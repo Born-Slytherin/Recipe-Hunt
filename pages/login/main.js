0b29d2eb0b1a2b62ef54f7367cdf49c24d2e3d11
Born-Slytherin <afsal786vn@gmail.com>
2024-08-14 12:50:02 +0530

    eye set

--- pages/login/main.js
+++ pages/login/main.js
@@ -4,10 +4,10 @@ let eye_closed = document.querySelector(".eye-closed");
 eye_open.addEventListener("click", function () {
   eye_closed.style.display = "none";
   eye_open.style.display = "block";
+  
 });
 
 eye_closed.addEventListener("click", function () {
-  console.log("clicked");
   eye_closed.style.display = "block";
   eye_open.style.display = "none";
 });
