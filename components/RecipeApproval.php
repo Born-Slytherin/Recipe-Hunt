bd6b0a286a5a543f91238061e554eba59436c9e5
yourusername <afsalvn47@gmail.com>
2025-01-03 10:59:44 +0530

    added dfds and er

--- components/RecipeApproval.php
+++ components/RecipeApproval.php
@@ -1,3 +1,14 @@
-<div>
-    Recipe Approval
+<div class="recipeApprovalContainer">
+    <span>No pending approvals</span>
 </div>
+<style>
+    .recipeApprovalContainer {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 24px;
+        color: #555;
+    }
+</style>
